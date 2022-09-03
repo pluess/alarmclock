953560dadf96ead57183fbd087b95c5ffd8b3400
Ernst Pluess <851732+pluess@users.noreply.github.com>
2022-09-03 08:19:48 +0200

    Numeric and symbolic character dictionary

--- c64german.py
+++ c64german.py
@@ -1,11 +1,57 @@
 from fontc64germanregular8 import fontC64GermanRegular8
 
+"""
+Provides a the C64 bitmap font with german characters.
+
+A-Z: 01-26
+0-9: 48-57
+colon(:): 58
+heart: 83
+A-Z inverse: 129-154
+0-9 inverse: 176-185
+colon(:) inverse: 186
+heart inverse: 211
+a-z: 257-282
+a-z inverse: 385-410
+
+Based on
+https://github.com/robhagemans/hoard-of-bitfonts
+https://github.com/robhagemans/monobit
+"""
 class C64German:
 
-	def chunker(self, seq, size):
+	def __init__(self):
+		self.numericCharacterList = self.readFont(False)
+		self.symbolicCharacterDict = self.mapCharacters(self.numericCharacterList)  # type: ignore
+
+	def __getitem__(self, key) -> list:
+		if isinstance(key, int):
+			return self.numericCharacterList.get(key)
+		else:
+			return self.symbolicCharacterDict.get(key)
+
+	def mapCharacters(self, characterList: dict)-> dict:
+		characterDict = {}
+		pos = 1
+		for i in range(ord('A'), ord('Z') + 1):  # type: ignore
+			characterDict[chr(i)] = characterList[pos]  # type: ignore
+			pos += 1
+		pos = 257
+		for i in range(ord('a'), ord('z') + 1):  # type: ignore
+			characterDict[chr(i)] = characterList[pos]  # type: ignore
+			pos += 1
+		pos = 48
+		for i in range(ord('0'), ord('9') + 1):  # type: ignore
+			characterDict[chr(i)] = characterList[pos]  # type: ignore
+			pos += 1
+		characterDict['heart'] = characterList[83] # type: ignore
+		characterDict['colon'] = characterList[58] # type: ignore
+		return characterDict
+
+	def chunker(self, seq: list, size: int):
 		return (seq[pos:pos + size] for pos in range(0, len(seq), size))  # type: ignore
 
-	def toCharField(self, byte):
+	def toCharField(self, byte: list) -> str:
 		binaryString = bin(byte)
 		binaryString = binaryString[2:]
 		l = len(binaryString)
@@ -15,18 +61,19 @@ class C64German:
 		binaryString = binaryString.replace('1', 'XX')
 		return binaryString
 
-	def readFont(self):
+	def readFont(self, output: bool) -> dict:
+		characterList = {}
 		counter = 0
-		for line in self.chunker(fontC64GermanRegular8, 8):
+		for character in self.chunker(fontC64GermanRegular8, 8):
+			characterList[counter] = character  # type: ignore
+			if output: print(str(counter) + ': '+ str(character))  # type: ignore
 			counter += 1
-			print(str(counter) + ': '+ str(line))  # type: ignore
-			for byte in line:
-				charString = self.toCharField(byte)
-				print(charString)
-    
-	def __init__(self, *argv, **kwargs):
-		pass
+			if output:
+				for byte in character:
+					charString = self.toCharField(byte)
+					print(charString)
+		return characterList
 
 if __name__ == '__main__':  # type: ignore
 	font = C64German()
-	font.readFont()
+	font.readFont(True)
--- c64germanunittest.py
+++ c64germanunittest.py
@@ -15,14 +15,33 @@ class C64GermanUnitTest(unittest.TestCase):
 		self.assertEqual(expectedSeqSeq[1], next(actualSeqSeq))  # type: ignore
 
 	def test_toCharField(self):
-		a =  self.c64German.toCharField(0b01010101)
+		a =  self.c64German.toCharField(0b01010101)  # type: ignore
 		self.assertEqual(a, '..XX..XX..XX..XX')
-		a =  self.c64German.toCharField(0b10101010)
+		a =  self.c64German.toCharField(0b10101010)  # type: ignore
 		self.assertEqual(a, 'XX..XX..XX..XX..')
-		a =  self.c64German.toCharField(0b00000000)
+		a =  self.c64German.toCharField(0b00000000)  # type: ignore
 		self.assertEqual(a, '................')
-		a =  self.c64German.toCharField(0b11111111)
+		a =  self.c64German.toCharField(0b11111111)  # type: ignore
 		self.assertEqual(a, 'XXXXXXXXXXXXXXXX')
+
+	def test_getitem_int(self):
+		actualCharacter = self.c64German[129]
+		expectedCharacter = [231, 195, 153, 153, 129, 153, 153, 255]
+		self.assertEqual(actualCharacter, expectedCharacter)
+
+	def test_getitem_symbolic(self):
+		actualCharacter = self.c64German['A']
+		expectedCharacter = [24, 60, 102, 102, 126, 102, 102, 0]
+		self.assertEqual(actualCharacter, expectedCharacter)
+
+	def test_mapCharacters(self):
+		actualCharacterDict = self.c64German.mapCharacters(self.c64German.numericCharacterList)
+		self.assertEqual(actualCharacterDict['A'], [24, 60, 102, 102, 126, 102, 102, 0])  # type: ignore
+		self.assertEqual(actualCharacterDict['Z'], [126, 6, 12, 24, 48, 96, 126, 0])  # type: ignore
+		self.assertEqual(actualCharacterDict['a'], [0, 0, 60, 6, 62, 102, 62, 0])  # type: ignore
+		self.assertEqual(actualCharacterDict['z'], [0, 0, 126, 12, 24, 48, 126, 0])  # type: ignore
+		self.assertEqual(actualCharacterDict['0'], [60, 102, 110, 118, 102, 102, 60, 0])  # type: ignore
+		self.assertEqual(actualCharacterDict['9'], [60, 102, 102, 62, 6, 102, 60, 0])  # type: ignore
 
 if __name__ == '__main__':  # type: ignore
 	unittest.main()
