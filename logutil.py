405dddc2a589d99efebc824b1a1d5a63f845faee
Ernst Pluess <ernst@pluess.li>
2022-09-25 06:10:36 +0200

    improoved log format

--- logutil.py
+++ logutil.py
@@ -4,11 +4,18 @@ from logging.handlers import RotatingFileHandler
 logging.basicConfig(level=logging.DEBUG)
 
 def get_logger(name: str) -> logging.Logger:
+    formatter = logging.Formatter(fmt='%(asctime)s %(levelname)s %(name)s %(message)s')
+    
     logger = logging.Logger(name)
     logger.setLevel(logging.DEBUG)
+
+    rotatingFileHandler = RotatingFileHandler('myloggfile.log', maxBytes=10000, backupCount=3)
+    rotatingFileHandler.setFormatter(formatter)
+    streamHandler = logging.StreamHandler()
+    streamHandler.setFormatter(formatter)
     
-    logger.addHandler(RotatingFileHandler('myloggfile.log', maxBytes=10000, backupCount=3))
-    logger.addHandler(logging.StreamHandler())
+    logger.addHandler(rotatingFileHandler)
+    logger.addHandler(streamHandler)
     
     return logger
 all = (get_logger)
